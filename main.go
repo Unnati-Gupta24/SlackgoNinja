a34c2ed1645d609d8cad47b1ce0e7ff159dd5bdd
Unnati gupta <guptaunnati031@gmail.com>
2024-09-15 23:00:48 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/slack-go/slack"
+)
+
+func main(){
+	os.Setenv("SLACK_BOT_TOKEN","xapp-1-A07MECABRB7-7733943621348-4e50e9de5dcf33c897b8975b4e56548a79307da0e70189541847719b1ca29da7")
+	os.Setenv("CHANNEL_ID","D07MKPE4PCL")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"linkedin.txt"}
+
+	for i := 0; i<len(fileArr); i++{
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File: fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil{
+			fmt.Printf("%s\n",err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s\n",file.Name,file.URL)
+	}
+
+}
